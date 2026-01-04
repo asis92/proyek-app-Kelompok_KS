5fba8f1433d950989f6a93f86ea59285be9e73eb
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2026-01-04 19:53:55 +0700

    Splash Screen Asis Step 3

--- aplikasi_tor_tor/lib/screen/splashscreen1.dart
+++ aplikasi_tor_tor/lib/screen/splashscreen1.dart
@@ -10,6 +10,21 @@ class Splashscreen1 extends StatefulWidget {
 class _Splashscreen1State extends State<Splashscreen1> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
