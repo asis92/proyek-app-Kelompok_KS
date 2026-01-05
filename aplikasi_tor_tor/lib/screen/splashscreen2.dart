7ce88d3084d6eb74aa12762751a49663e1032fd7
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-01-05 20:17:36 +0700

    UAS Grafica jati Sugiyarto Step 4

--- aplikasi_tor_tor/lib/screen/splashscreen2.dart
+++ aplikasi_tor_tor/lib/screen/splashscreen2.dart
@@ -6,18 +6,24 @@ class MySplashScreenUAS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey,
-            ),
-          )
-        ],
+      appBar: AppBar(
+        title: const Text("Tugas UAS"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey,
+                
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
