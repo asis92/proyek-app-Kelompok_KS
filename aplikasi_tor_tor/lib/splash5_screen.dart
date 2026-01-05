6d49b622fa0fd160b0716242457e9b8f91eee677
Jejen Jaenudin <1125170031@global.ac.id>
2026-01-05 08:11:28 +0700

    Menambahkan tampilan splash screen dengan animasi fade

--- aplikasi_tor_tor/lib/splash5_screen.dart
+++ aplikasi_tor_tor/lib/splash5_screen.dart
@@ -7,9 +7,10 @@ class SplashScreen5 extends StatefulWidget {
   State<SplashScreen5> createState() => _SplashScreen5State();
 }
 
-class _SplashScreen5State extends State<SplashScreen5> 
-with SingleTickerProviderStateMixin {
+class _SplashScreen5State extends State<SplashScreen5>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+   late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
@@ -28,10 +29,77 @@ with SingleTickerProviderStateMixin {
       }
     });
   }
-   @override
+
+  @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
-    
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+          ),
+        ),
+      ),
+    );
+  }
+   @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+          ),
+        ),
+        child: Center(
+            child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                        const Icon(Icons.hotel, size: 100, color: Colors.white),
+                  const SizedBox(height: 24),
+                  const Text(
+                    'Booking Hotel',
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Temukan Menginap Impianmu',
+                    style: TextStyle(fontSize: 20, color: Colors.white70),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(height: 20),
+
+                  Container(
+                        height: 10,
+                        width: 10,
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 7, 243, 109),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ]
+                )
+            )
+        ),
+      ),
+    );
+  }
 }
