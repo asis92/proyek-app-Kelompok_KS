db7465dae64c388d293b10e181f6a8d39ca39831
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-01-05 19:48:34 +0700

    UAS Grafica jati Sugiyarto Step 1

---
+++ aplikasi_tor_tor/lib/screen/splashscreen2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreenUAS extends StatelessWidget {
+  const MySplashScreenUAS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    )
+  }
+}
