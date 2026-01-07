191d0a096770588b1fb3c332236f428a1b5035b9
Jejen Jaenudin <1125170031@global.ac.id>
2026-01-07 17:04:51 +0700

    update main

--- aplikasi_tor_tor/lib/main.dart
+++ aplikasi_tor_tor/lib/main.dart
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import 'package:aplikasi_tor_tor/screen/splashscreen1.dart';
-=======
-import 'package:aplikasi_tor_tor/dataprofile/profile1.dart';
->>>>>>> 8a23a53d78c9c5244c194d0cbb6f291259b3703a
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,11 +17,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
+
       home: Splashscreen1(), //memanggil nama class splash screen 1
-=======
-      home: Card1(), //memanggil nama class splash screen 1
->>>>>>> 8a23a53d78c9c5244c194d0cbb6f291259b3703a
+
     );
   }
 }
