2996b80a63e31e495068dfa0d465862715c75519
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-01-07 21:29:17 +0700

    UAS GraficaJS_Profile Step 2

--- aplikasi_tor_tor/lib/dataprofile/profile2.dart
+++ aplikasi_tor_tor/lib/dataprofile/profile2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class DataProfile extends StatelessWidget {
+  const DataProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
