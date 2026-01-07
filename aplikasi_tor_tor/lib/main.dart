4e4a06f4af191e1a8db142b86dbb8fce74ce512c
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2026-01-07 15:43:59 +0700

    Perbaikan Folder Data Profile Mas Jejen Step 1

--- aplikasi_tor_tor/lib/main.dart
+++ aplikasi_tor_tor/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aplikasi_tor_tor/dataprofile/profile3.dart';
+import 'package:aplikasi_tor_tor/dataprofile/profile5.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
       debugShowCheckedModeBanner: false,
-      home: TeamProfilePage(), //memanggil nama class splash screen 1
+      home: Profile(), //memanggil nama class splash screen 1
     );
   }
 }
