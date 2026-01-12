6321c7b48d7899d8a76c535a166a18a83413c7a4
arisandibulan-boop <arisandibulan@gmail.com>
2026-01-12 21:28:54 +0700

    ubah menjadi StatefulWidget

--- aplikasi_tor_tor/notifcationdemo/lib/home_page.dart
+++ aplikasi_tor_tor/notifcationdemo/lib/home_page.dart
@@ -4,4 +4,8 @@ import 'package:firebase_messaging/firebase_messaging.dart'; // Import Firebase 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  final String title; // tambah title untuk judul halaman
+  final String title; // tambah title untuk judul 
+  
+   @override
+  State<MyHomePage> createState() => _MyHomePageState(); // ubah menjadi StatefulWidget
+}
