03b6824c0ec508c9e539baf010b07a51b005de0d
arisandibulan-boop <arisandibulan@gmail.com>
2026-01-12 21:28:00 +0700

    tambah title untuk judul halaman

--- aplikasi_tor_tor/notifcationdemo/lib/home_page.dart
+++ aplikasi_tor_tor/notifcationdemo/lib/home_page.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart'; // Import Firebase Messaging package
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title; // tambah title untuk judul halaman
