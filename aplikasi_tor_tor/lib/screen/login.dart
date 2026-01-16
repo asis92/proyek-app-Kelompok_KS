058955e9a43c0d1cc60c5d67a24f1b062add7563
Anggie991 <1125170030@gmail.com>
2026-01-16 11:20:19 +0700

    Login 3 Anggie

--- aplikasi_tor_tor/lib/screen/login.dart
+++ aplikasi_tor_tor/lib/screen/login.dart
@@ -13,37 +13,11 @@ class LoginPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: 100),
-              const Text(
-                'Login',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
               const SizedBox(height: 40),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
+
+              // Icon
+              const Icon(Icons.lock_outline, size: 80, color: Colors.green),
               const SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle login logic here
-                },
-                child: const Text('Login'),
-              ),
             ],
           ),
         ),
